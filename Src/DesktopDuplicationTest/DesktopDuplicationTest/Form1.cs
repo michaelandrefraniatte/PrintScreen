cd9896aa4879f9053cce2f959b2bdd7273fc6392
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-05-02 07:32:24 +0200

    Add a test program

---
+++ Src/DesktopDuplicationTest/DesktopDuplicationTest/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DesktopDuplication;
+
+namespace DesktopDuplicationTest
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("winmm.dll", EntryPoint = "timeBeginPeriod")]
+        public static extern uint TimeBeginPeriod(uint ms);
+        [DllImport("winmm.dll", EntryPoint = "timeEndPeriod")]
+        public static extern uint TimeEndPeriod(uint ms);
+        [DllImport("ntdll.dll", EntryPoint = "NtSetTimerResolution")]
+        public static extern void NtSetTimerResolution(uint DesiredResolution, bool SetResolution, ref uint CurrentResolution);
+        public static uint CurrentResolution = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private DesktopDuplicator desktopDuplicator;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TimeBeginPeriod(1);
+            NtSetTimerResolution(1, true, ref CurrentResolution);
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            try
+            {
+                desktopDuplicator = new DesktopDuplicator(0);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            Task.Run(() => CopyScreen());
+        }
+        private void CopyScreen()
+        {
+            while (true)
+            {
+                Application.DoEvents();
+                DesktopFrame frame = null;
+                try
+                {
+                    frame = desktopDuplicator.GetLatestFrame();
+                }
+                catch
+                {
+                    desktopDuplicator = new DesktopDuplicator(0);
+                    continue;
+                }
+                if (frame != null)
+                {
+                    this.BackgroundImage = null;
+                    this.BackgroundImage = frame.DesktopImage;
+                }
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
